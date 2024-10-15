113bb3f4c45ec82d71231d7210ce62576c0b961f
Hopertz <lugano.ngulwa@gmail.com>
2024-10-15 23:55:58 +0300

    make knight moves

--- src/moves/moves.js
+++ src/moves/moves.js
@@ -1,7 +1,15 @@
+import { getKnightMoves } from "./knight";
 import { getRookMoves } from "./rook";
 
 export const moves = {
     getRegularMoves : function(position, piece, rank, file) {
-        return getRookMoves(position, piece, rank, file)
+
+        if (piece.endsWith("n")){
+            return getKnightMoves(position, piece, rank, file)
+        }
+        if (piece.endsWith("r")){
+            return getRookMoves(position, piece, rank, file)
+        }
+        
     }
 }
