398a30b0218044424c3bf5380982d9bffabc51ce
Hopertz <lugano.ngulwa@gmail.com>
2024-10-16 09:28:00 +0300

    refactor code

--- src/moves/queen.js
+++ src/moves/queen.js
@@ -2,6 +2,9 @@ import { getRookMoves } from "./rook";
 import {getBishopMoves} from "./bishop"
 
 export const getQueenMoves = (position, piece, rank, file) => {
-  return [...getBishopMoves(position, piece, rank, file),...getRookMoves(position, piece, rank, file)]
+  return [
+           ...getBishopMoves(position, piece, rank, file),
+           ...getRookMoves(position, piece, rank, file)
+        ]
 
 }
