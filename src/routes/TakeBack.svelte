86099a5acf2937a29afeaaed69b53e6be9e6632a
Hopertz <lugano.ngulwa@gmail.com>
2024-11-08 10:30:32 +0300

    fix: onpromotion takeback bug

--- src/routes/TakeBack.svelte
+++ src/routes/TakeBack.svelte
@@ -1,11 +1,15 @@
 <script>
-	import { game } from '../store/store';
+	import { game, Statuses } from '../store/store';
 
 	const takeBack = () => {
 		if ($game.positions.length > 1) {
+			const slicedPostns = $game.positions.slice(0, $game.positions.length - 1)
+			slicedPostns[slicedPostns.length - 1].status = Statuses.ongoing
+			slicedPostns[slicedPostns.length - 1].promotionValues = { rank: 0, file: 0, x: 0, y: 0 }
 			game.set({
-				positions: $game.positions.slice(0, $game.positions.length - 1),
-				turn: $game.turn === 'w' ? 'b' : 'w'
+				positions: slicedPostns,
+				turn: $game.turn === 'w' ? 'b' : 'w',
+				possibleMoves: []
 			});
 		}
 	};
